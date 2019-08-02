7c84c001522ad6cdab02107f9ce5a55f3e161685
Teakowa <git@teakowa.com>
2019-08-02 18:16:52 +0800

    fix(Headers): change `$headers` to private

--- src/Adapter/Headers.php
+++ src/Adapter/Headers.php
@@ -2,13 +2,31 @@
 
 namespace Teakowa\Octo\Adapter;
 
+/**
+ * Class Headers
+ *
+ * @package Teakowa\Octo\Adapter
+ */
 class Headers implements Header
 {
+    /**
+     * @var array
+     */
+    private $headers;
+
+    /**
+     * Headers constructor.
+     *
+     * @param  array  $headers
+     */
     public function __construct(array $headers)
     {
         $this->headers = $headers;
     }
 
+    /**
+     * @return array
+     */
     public function getHeaders(): array
     {
         return $this->headers;
