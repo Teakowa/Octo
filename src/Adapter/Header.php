5175f7f4f9580bac33f5cdb1220304770b4e507d
Teakowa <git@teakowa.com>
2019-08-06 11:31:14 +0800

    feat(Headers): add `getProvider` function, it's can be chosen     provider headers by default     example: `$header = (new Headers())->getProvider('Tencent')`;

--- src/Adapter/Header.php
+++ src/Adapter/Header.php
@@ -2,7 +2,23 @@
 
 namespace Teakowa\Octo\Adapter;
 
+/**
+ * Interface Header
+ *
+ * @package Teakowa\Octo\Adapter
+ */
 interface Header
 {
+    /**
+     * @return array
+     */
     public function getHeaders(): array;
+
+    /**
+     * @param  string  $provider
+     *
+     * @return array
+     * @since 1.2.1
+     */
+    public function getProvider(string $provider): array;
 }
--- src/Adapter/Headers.php
+++ src/Adapter/Headers.php
@@ -29,4 +29,45 @@ class Headers implements Header
     {
         return $this->headers;
     }
+
+    /**
+     * @param  string  $provider
+     *
+     * @return array
+     * @since 1.2.1
+     */
+    public function getProvider(string $provider): array
+    {
+        switch ($provider) {
+            case 'Kugou':
+                $this->headers =
+                    ['User-Agent' => 'Mozilla/5.0 (Linux; Android 5.1; MZ-m1 metal Build/LMY47I) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0',];
+                break;
+            case 'Tencent':
+                $this->headers = [
+                    'Referer'         => 'https://y.qq.com/',
+                    'Cookie'          => 'pgv_pvi=22038528; pgv_si=s3156287488; pgv_pvid=5535248600; yplayer_open=1; ts_last=y.qq.com/portal/player.html; ts_uid=4847550686; yq_index=0; qqmusic_fromtag=66; player_exist=1',
+                    'User-Agent'      => 'QQ%E9%9F%B3%E4%B9%90/54409 CFNetwork/901.1 Darwin/17.6.0 (x86_64)',
+                    'Accept'          => '*/*',
+                    'Accept-Language' => 'zh-CN,zh;q=0.8,gl;q=0.6,zh-TW;q=0.4',
+                    'Connection'      => 'keep-alive',
+                    'Content-Type'    => 'application/x-www-form-urlencoded',
+                ];
+                break;
+            case 'Netease':
+                $this->headers = [
+                    'Referer'         => 'https://music.163.com/',
+                    'Cookie'          => 'appver=1.5.9; os=osx; __remember_me=true; osver=%E7%89%88%E6%9C%AC%2010.13.5%EF%BC%88%E7%89%88%E5%8F%B7%2017F77%EF%BC%89;',
+                    'User-Agent'      => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/605.1.15 (KHTML, like Gecko)',
+                    'X-Real-IP'       => long2ip(mt_rand(1884815360, 1884890111)),
+                    'Accept'          => '*/*',
+                    'Accept-Language' => 'zh-CN,zh;q=0.8,gl;q=0.6,zh-TW;q=0.4',
+                    'Connection'      => 'keep-alive',
+                    'Content-Type'    => 'application/x-www-form-urlencoded',
+                ];
+                break;
+        }
+
+        return $this->headers;
+    }
 }
